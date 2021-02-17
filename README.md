e418581d4bc4e48dbe97d498ab2294df00ddc90d
Soumesh Banerjee <soumeshbanerjee@live.com>
2021-02-17 19:47:04 +0530

    Added Favicon and Update Readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # no-news
-Norway News API (for regjeringen.no) and a light weight UI
+
+Norway News API `un-official` (for regjeringen.no) and a light weight UI
+
+_(Please check out the [live demo](https://no-news.herokuapp.com))_
+
+## Getting Started
+
+To run the server follow these steps:
+
+- Install the dependencies
+
+  ```bash
+    $ yarn
+    // or
+    $ npm install
+  ```
+
+- Run Server (`$ yarn start` or `$ npm start`)
+
+> The port of the server is set to `env:PORT` if not set defaults to `8080`
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 require("pug")
 const express = require("express")
-const { fetchNews } = require("./news")
+const favicon = require('express-favicon')
+const { fetchNews } = require("./src/news")
 const PORT = process.env.PORT || 8080
 
 const app = express()
-
-app.set("view engine", "pug")
-
-app.get("/", async (req, res) => {
-  try {
-    const startTime = Date.now()
-    const page = req.query.page || 1;
-    const news = await fetchNews(page);
-    const duration = Date.now() - startTime;
-    console.log(`Found ${news ? news.length : 0} news articles in ${duration}ms`);
-    res.render("index", { news, page, duration })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ ok: false, error: "Failed to get the news" })
-  }
-}).listen(PORT, () => console.log(`Server Started on ${PORT}`))
+app
+  .set("view engine", "pug")
+  .use(favicon(__dirname + "/public/favicon.ico"))
+  .get("/", async (req, res) => {
+    try {
+      const startTime = Date.now()
+      const page = req.query.page || 1;
+      const news = await fetchNews(page);
+      const duration = Date.now() - startTime;
+      console.log(`Found ${news ? news.length : 0} news articles in ${duration}ms`);
+      res.render("index", { news, page, duration })
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ ok: false, error: "Failed to get the news" })
+    }
+  }).listen(PORT, () => console.log(`Server Started on ${PORT}`))
